f8ac08511c1e97adc92623b67ff94bdd9ab5f6be
pR0Ps <pR0Ps.CM@gmail.com>
2013-12-03 17:03:35 -0500

    Basic functionality

---
+++ README.md
@@ -0,0 +1,17 @@
+Mag2Tor
+=======
+
+A simple webapp that takes a magnet URI and turns it into a downloadable torrent file.
+
+Setup
+-----
+
+1. Set up a virtual environment (Python 2 or 3 will work)
+2. Activate the virtual environment
+3. Install all requirements( `pip install -r requirements.txt` )
+4. Run the application ( `python mag2tor.py` )
+
+Usage
+-----
+
+Visit `http://[host]/?l=[magnet link]` in a browser
---
+++ mag2tor.py
@@ -0,0 +1,25 @@
+from bottle import route, run, request, redirect
+import re
+
+HASH_RE = re.compile(".+xt=urn:btih:([^&]+).*")
+TOR_CACHE = "http://torrage.com/torrent/{}.torrent"
+
+@route('/')
+def index():
+    link = request.query.get("l")
+    if not link:
+        return "<html><b>No link provided<br/><br/>Try http://{}/?l=[magnet link]</b></html>".format(request.urlparts.netloc)
+
+    temp = HASH_RE.match(link)
+    if not temp:
+        return "<html><b>Invalid link provided</b></html>"
+
+    do_link(temp.group(1))
+
+def do_link(info_hash):
+    # Can the filename be specified if something other than a redirect is used?
+
+    redirect(TOR_CACHE.format(info_hash.upper()))
+
+if __name__ == "__main__":
+    run(host='0.0.0.0', port=8080)
