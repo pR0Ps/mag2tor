41dcb4b5ce5d469c684fe769f955a41cebd7d977
Graham McGregor <haxxor42@gmail.com>
2014-02-11 20:34:40 -0500

    interactive error page

--- app.py
+++ app.py
@@ -6,15 +6,29 @@ TOR_CACHE = "http://torrage.com/torrent/{}.torrent"
 
 app = application = Bottle()
 
+htmlblob = """<html>
+                    <b><script type='text/javascript'>
+                        function loadMagLink(frm){
+                            window.location.assign('http://%s/?l=' + frm.maglink.value)
+                        }
+                    </script>
+                    <form>
+                        <p>%s<br/><br/>Needs to be of format http://%s/?l=[magnet link]</p>
+                        Or, Enter the magnet link: <input type='text' name=maglink>
+                        <input type='button' value='Convert' onclick='loadMagLink(this.form)'>
+                    </form></b>
+                </html>
+           """
+
 @app.route('/')
 def index():
     link = request.query.get("l")
     if not link:
-        return "<html><b>No link provided<br/><br/>Try http://{}/?l=[magnet link]</b></html>".format(request.urlparts.netloc)
+        return htmlblob % (request.urlparts.netloc, "No link provided", request.urlparts.netloc)
 
     temp = HASH_RE.match(link)
     if not temp:
-        return "<html><b>Invalid link provided</b></html>"
+        return htmlblob % (request.urlparts.netloc, "Invalid link provided", request.urlparts.netloc)
 
     do_link(temp.group(1))
 
