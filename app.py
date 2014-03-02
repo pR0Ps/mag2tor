6cbbc16fc98417d865aeb6685ec6eabcf8b8e2c0
pR0Ps <pR0Ps.CM@gmail.com>
2014-03-01 19:59:47 -0500

    Removed need for JS, fixed issue with enter vs. click button

--- app.py
+++ app.py
@@ -7,15 +7,10 @@ TOR_CACHE = "http://torrage.com/torrent/{}.torrent"
 app = application = Bottle()
 
 htmlblob = """<html>
-                    <b><script type='text/javascript'>
-                        function loadMagLink(frm){
-                            window.location.assign('http://%s/?l=' + frm.maglink.value)
-                        }
-                    </script>
-                    <form>
-                        <p>%s<br/><br/>Needs to be of format http://%s/?l=[magnet link]</p>
-                        Or, Enter the magnet link: <input type='text' name=maglink>
-                        <input type='button' value='Convert' onclick='loadMagLink(this.form)'>
+                    <b><form>
+                        <p>{0}<br/><br/>Needs to be of format http://{1}/?l=[magnet link]</p>
+                        Or, Enter the magnet link: <input type='text' name=l>
+                        <input type='Submit' value='Convert'>
                     </form></b>
                 </html>
            """
@@ -24,18 +19,13 @@ htmlblob = """<html>
 def index():
     link = request.query.get("l")
     if not link:
-        return htmlblob % (request.urlparts.netloc, "No link provided", request.urlparts.netloc)
+        return htmlblob.format("No link provided", request.urlparts.netloc)
 
     temp = HASH_RE.match(link)
     if not temp:
-        return htmlblob % (request.urlparts.netloc, "Invalid link provided", request.urlparts.netloc)
+        return htmlblob.format("Invalid link provided", request.urlparts.netloc)
 
-    do_link(temp.group(1))
-
-def do_link(info_hash):
-    # Can the filename be specified if something other than a redirect is used?
-
-    redirect(TOR_CACHE.format(info_hash.upper()))
+    redirect(TOR_CACHE.format(temp.group(1).upper()))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
