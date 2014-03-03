a618e67e0f633e604d6d888ce0098d2c27c2e378
pR0Ps <pR0Ps.CM@gmail.com>
2014-03-03 18:39:44 -0500

    Integrate error messages

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ TOR_CACHE = "http://torrage.com/torrent/{}.torrent"
 
 app = application = Bottle()
 
-htmlblob = """
+HTMLBLOB = """
 <DOCTYPE HTML>
 <html>
   <head>
@@ -25,15 +25,15 @@ htmlblob = """
         text-align: center;
         margin-top: 100px;
       }}
+      a#title{{
+        color: #000000;
+        text-decoration: none;
+      }}
       img#gh-banner {{
         position: absolute;
         top: 0;
         right: 0;
         border: 0;
-      }}
-      p#error {{
-        color: #FF0000;
-        display: none; /* for now */
       }}
       footer {{
           position: absolute;
@@ -44,47 +44,51 @@ htmlblob = """
         padding:5px 15px;
         -webkit-border-radius: 5px;
         border-radius: 5px;
+        border:2px solid
       }}
       input[type=text] {{
         width: 570px;
-        border:2px solid
-        border-color: #ccc;
+        border-color: #555555;
       }}
-      input[type=text]:focus {{
-        border-color:#333;
+      input[type=text].error {{
+          border-color: #FF0000;
       }}
       input[type=submit] {{
         cursor:pointer;
-        background:#ccc;
-        border: 2px none;
+        font-weight: bold;
+        background:#C0C0C0;
         margin: 10px 5px 10px 5px;
-        height: 30px;
       }}
     </style>
   </head>
   <body>
     <main>
-      <h1>Mag2Tor</h1>
+      <a href="/" id='title'><h1>Mag2Tor</h1></a>
       <a href="https://github.com/pR0Ps/mag2tor"><img id="gh-banner" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
       <p>Enter a magnet link below to be redirected to the corresponding torrent file.</p>
       <form>
-        <input type='text' placeholder='Magnet link' name='l'><br>
+        <input type='text' placeholder='{0}' name='l' {1}><br>
         <input type='Submit' value='Convert'>
       </form>
     </main>
-    <p id='error' >{0}</p>
     <footer>Made by <a href="http://cmetcalfe.ca">Carey Metcalfe</a></footer>
 </html>"""
+
+def display(error_text=None):
+    if error_text:
+        return HTMLBLOB.format("ERROR: {0}".format(error_text), "class='error'")
+    else:
+        return HTMLBLOB.format("Enter a magnet link", "")
 
 @app.route('/')
 def index():
     link = request.query.get("l")
     if not link:
-        return htmlblob.format("")
+        return display()
 
     temp = HASH_RE.match(link)
     if not temp:
-        return htmlblob.format("ERROR: Invalid link provided")
+        return display("Invalid link provided")
 
     redirect(TOR_CACHE.format(temp.group(1).upper()))
 
